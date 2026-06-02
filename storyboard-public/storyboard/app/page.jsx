463b33bf08a675eab150e919a693b1f4ebc7c394
Onur_Starz <onurgazioglu009@gmail.com>
2026-06-02 21:14:39 +0300

    Delete storyboard-public/storyboard directory

--- storyboard-public/storyboard/app/page.jsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const StoryboardRoot = dynamic(() => import("./StoryboardRoot"), { ssr: false });
-
-export default function Page() {
-  return <StoryboardRoot />;
-}
